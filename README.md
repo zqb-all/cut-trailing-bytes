2505d5beb9b5b8066ee94eab7f451ee3755989a1
zhuangqiubin <zhuangqiubin@gmail.com>
2020-04-05 18:12:09 +0800

    init verison

---
+++ README.md
@@ -0,0 +1,58 @@
+cut-trailing-bytes - a tool to cut tailing bytes.
+
+# Example
+
+```bash
+$ echo "hello" > hello_00
+
+$ dd if=/dev/zero bs=1k count=1 >> hello_00
+1+0 records in
+1+0 records out
+1024 bytes (1.0 kB, 1.0 KiB) copied, 0.0031857 s, 321 kB/s
+
+$ hexdump -C hello_00
+00000000  68 65 6c 6c 6f 0a 00 00  00 00 00 00 00 00 00 00  |hello...........|
+00000010  00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00  |................|
+*
+00000406
+
+$ du -b hello_00
+1030    hello_00
+
+$ ./cut-trailing-bytes hello_00
+cut hello_00 from 1030 to 6
+
+$ hexdump -C hello_00
+00000000  68 65 6c 6c 6f 0a                                 |hello.|
+00000006
+
+$ du -b hello_00
+6       hello_00
+
+$ echo "hello" > hello_ff
+
+$ dd if=/dev/zero bs=1k count=1 | tr '\000' '\377' >> hello_ff
+1+0 records in
+1+0 records out
+1024 bytes (1.0 kB, 1.0 KiB) copied, 0.0070723 s, 145 kB/s
+
+$ hexdump -C hello_ff
+00000000  68 65 6c 6c 6f 0a ff ff  ff ff ff ff ff ff ff ff  |hello...........|
+00000010  ff ff ff ff ff ff ff ff  ff ff ff ff ff ff ff ff  |................|
+*
+00000406
+
+$ du -b hello_ff
+1030    hello_ff
+
+$ ./cut-trailing-bytes hello_ff -c ff
+cut hello_ff from 1030 to 6
+
+$ hexdump -C hello_ff
+00000000  68 65 6c 6c 6f 0a                                 |hello.|
+00000006
+
+zqb@WSL:~/workspace/rust/cut-trailing-bytes
+$ du -b hello_ff
+6       hello_ff
+```
---
+++ src/main.rs
@@ -0,0 +1,63 @@
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::path::PathBuf;
+use structopt::StructOpt;
+use std::num::ParseIntError;
+
+fn parse_hex(s: &str) -> Result<u8, ParseIntError> {
+    u8::from_str_radix(s, 16)
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "cut-tailing-bytes", about = "A tool for cut tailing bytes, default cut tailing NULL bytes(0x00 in hex)")]
+struct Opt {
+    /// File to cut
+    #[structopt(parse(from_os_str))]
+    file: PathBuf,
+
+    /// For example, pass 'ff' if want to cut 0xff
+    #[structopt(short = "c", long = "cut-byte", default_value="0", parse(try_from_str = parse_hex))]
+    byte_in_hex: u8,
+
+    /// Check the file but don't real cut it
+    #[structopt(short, long = "dry-run")]
+    dry_run: bool,
+}
+
+
+fn main() -> io::Result<()> {
+
+    let opt = Opt::from_args();
+    let filename = &opt.file;
+    let mut f = File::open(filename)?;
+    let mut valid_len = 0;
+    let mut tmp_len = 0;
+    let mut buffer = [0; 4096];
+
+    loop {
+        let mut n = f.read(&mut buffer[..])?;
+        if n == 0 { break; }
+        for byte in buffer.bytes() {
+            match byte.unwrap() {
+                byte if byte == opt.byte_in_hex => { tmp_len += 1; }
+                _ => {
+                    valid_len += tmp_len;
+                    tmp_len = 0;
+                    valid_len += 1;
+                }
+            }
+            n -= 1;
+            if n == 0 { break; }
+        }
+    }
+    if !opt.dry_run {
+        let f = OpenOptions::new().write(true).open(filename);
+        f.unwrap().set_len(valid_len)?;
+    }
+    println!("cut {} from {} to {}", filename.display(), valid_len + tmp_len, valid_len);
+
+    Ok(())
+}
+
